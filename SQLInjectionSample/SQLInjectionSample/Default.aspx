df7da0474c85cf97a563598ae1864f5ed983826b
ArghyaB <ArghyaB@RSLPTP995>
2019-09-06 18:22:50 +0530

    SQL Injection

---
+++ SQLInjectionSample/SQLInjectionSample/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SQLInjectionSample.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%if (User.Identity.IsAuthenticated)
+                { %>
+            Welcome : <%:User.Identity.Name %>
+            <br />
+            <a href="Login.aspx" >Logout</a>
+            <%} %>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SQLInjectionSample/SQLInjectionSample/Login.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQLInjectionSample
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private string userName;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Logout();
+            }
+        }
+
+        private void Logout()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (IsValidUser())
+            {
+                FormsAuthentication.RedirectFromLoginPage(this.userName, false);
+            }
+            else
+            {
+                lblMessage.Text = "Invalid Username or Password";
+            }
+        }
+
+        
+        private bool IsValidUser()
+        {
+            string commandText = $"SELECT * FROM Users WHERE Username = '{txtUsername.Text}' AND Password = '{txtPassword.Text}'";
+
+            // After Fixes
+            //string commandText = $"SELECT * FROM Users WHERE Username = @Username AND Password = @Password";
+
+            bool isValid = false;
+            using (SqlConnection oCnn = new SqlConnection(ConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString))
+            {
+                oCnn.Open();
+                using (SqlCommand oCmd = new SqlCommand())
+                {
+                    oCmd.Connection = oCnn;
+                    oCmd.CommandText = commandText;
+
+                    /* // After Fixes
+                    oCmd.Parameters.Add("Username", SqlDbType.VarChar, 63);
+                    oCmd.Parameters["Username"].Value = txtUsername.Text;
+
+                    oCmd.Parameters.Add("Password", SqlDbType.VarChar, 127);
+                    oCmd.Parameters["Password"].Value = txtPassword.Text;
+                    */
+                    oCmd.CommandType = System.Data.CommandType.Text;
+                    using (SqlDataReader oReader = oCmd.ExecuteReader())
+                    {
+                        while (oReader.Read())
+                        {
+                            userName = Convert.ToString(oReader["Name"]);
+                            isValid = true;
+                        }
+                    }
+                }
+            }
+
+            return isValid;
+        }
+
+    }
+}
